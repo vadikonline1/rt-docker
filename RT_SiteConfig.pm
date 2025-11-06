7b47f5185897c6f637eb6ae6731c5f9cd2defe3b
vadikonline1 <vadikonline1@gmail.com>
2025-11-06 18:00:41 +0200

    Add initial RT_SiteConfig.pm configuration

---
+++ RT_SiteConfig.pm
@@ -0,0 +1,14 @@
+Set($DatabaseType, "Pg");
+Set($DatabaseHost, "db");
+Set($DatabaseName, "rt");
+Set($DatabaseUser, "rtuser");
+Set($DatabasePassword, "yoymtMzViTqH3gdNzQn0yxASFx9b");
+
+Set($WebPath, "/");
+Set($WebPort, 80);
+Set($WebBaseURL, "http://localhost:8080");
+
+# Alte setări recomandate
+Set($Timezone, 'Europe/Bucharest');
+Set($OwnerEmail, 'admin@example.com');
+Set($Organization, 'My Company');
