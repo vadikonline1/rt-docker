a743360f78a44c993179bfffa5eec1a1eef1431c
vadikonline1 <vadikonline1@gmail.com>
2025-11-07 09:00:51 +0200

    Update RT_SiteConfig.pm

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -4,11 +4,12 @@ Set($DatabaseName, "rt");
 Set($DatabaseUser, "rtuser");
 Set($DatabasePassword, "yoymtMzViTqH3gdNzQn0yxASFx9b");
 
-Set($WebPath, "/");
 Set($WebPort, 80);
+Set($WebPath, "/");
 Set($WebBaseURL, "http://localhost:8080");
 
-# Alte setări recomandate
-Set($Timezone, 'Europe/Bucharest');
-Set($OwnerEmail, 'admin@example.com');
-Set($Organization, 'My Company');
+Set($Organization, "ExampleCorp");
+Set($Timezone, "Europe/Bucharest");
+Set($OwnerEmail, "admin@example.com");
+
+1;
