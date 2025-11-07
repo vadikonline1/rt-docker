1ab92edb07befb94eec1905eef172e588d08e481
vadikonline1 <vadikonline1@gmail.com>
2025-11-07 10:17:56 +0200

    Update RT_SiteConfig.pm

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -1,19 +1,19 @@
-Set($rtname, '$RT_RTNAME');
-Set($Organization, '$RT_ORGANIZATION');
-Set($WebDomain, '$RT_WEB_DOMAIN');
-Set($WebPort, '$RT_WEB_PORT');
+Set($rtname, $ENV{'RT_RTNAME'});
+Set($Organization, $ENV{'RT_ORGANIZATION'});
+Set($WebDomain, $ENV{'RT_WEB_DOMAIN'});
+Set($WebPort, $ENV{'RT_WEB_PORT'});
 Set($WebPath, '');
-Set($DatabaseUser, '$RT_DB_USER');
-Set($DatabasePassword, '$RT_DB_PASSWORD');
-Set($DatabaseHost, '$POSTGRES_HOST');
+
+Set($DatabaseUser, $ENV{'POSTGRES_USER'});
+Set($DatabasePassword, $ENV{'POSTGRES_PASSWORD'});
+Set($DatabaseHost, $ENV{'POSTGRES_HOST'});
 Set($DatabasePort, undef);
-Set($DatabaseName, '$RT_DB_NAME');
-Set($DatabaseAdmin, '$POSTGRES_USER');
+Set($DatabaseName, $ENV{'POSTGRES_DB'});
+Set($DatabaseAdmin, $ENV{'POSTGRES_USER'});
 
 Set($LogToSyslog, 'warning');
 Set($LogToSTDERR, 'warning');
 
-Set(%GnuPG, 'Enable' => '0');
-Set(%SMIME, 'Enable' => '0');
-
+Set(%GnuPG, 'Enable' => 0);
+Set(%SMIME, 'Enable' => 0);
 1;
