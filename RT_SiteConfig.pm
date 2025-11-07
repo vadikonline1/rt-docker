789049e56104a2817e028edbc3d078887c0352a9
vadikonline1 <vadikonline1@gmail.com>
2025-11-07 09:58:26 +0200

    Update RT_SiteConfig.pm

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -1,18 +1,14 @@
-# /opt/rt6/etc/RT_SiteConfig.pm
-
-Set($rtname, $ENV{'RT_RTNAME'});
-Set($Organization, $ENV{'RT_ORG_NAME'});
-Set($WebDomain, $ENV{'RT_WEB_DOMAIN'});
-Set($WebPort, $ENV{'RT_WEB_PORT'});
-Set($WebPath, $ENV{'RT_WEB_PATH'});
-
-Set($DatabaseType, $ENV{'RT_DB_TYPE'});
-Set($DatabaseUser, $ENV{'RT_DB_USER'});
-Set($DatabasePassword, $ENV{'RT_DB_PASS'});
-Set($DatabaseHost, $ENV{'RT_DB_HOST'});
-Set($DatabasePort, $ENV{'RT_DB_PORT'});
-Set($DatabaseName, $ENV{'RT_DB_NAME'});
-Set($DatabaseAdmin, $ENV{'RT_DB_ADMIN'});
+Set($rtname, '$RT_RTNAME');
+Set($Organization, '$RT_ORGANIZATION');
+Set($WebDomain, '$RT_WEB_DOMAIN');
+Set($WebPort, '$RT_WEB_PORT');
+Set($WebPath, '');
+Set($DatabaseUser, '$RT_DB_USER');
+Set($DatabasePassword, '$RT_DB_PASSWORD');
+Set($DatabaseHost, '$POSTGRES_HOST');
+Set($DatabasePort, undef);
+Set($DatabaseName, '$RT_DB_NAME');
+Set($DatabaseAdmin, '$POSTGRES_USER');
 
 Set($LogToSyslog, 'warning');
 Set($LogToSTDERR, 'warning');
